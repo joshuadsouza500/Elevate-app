03635f5bce0dbcd657cc68b8bac463d4a86a0b01
Joshua Dsouza <118182542+joshuadsouza500@users.noreply.github.com>
2025-03-15 17:29:59 +0300

    Update Hero2.tsx

--- app/components/sections/Hero2.tsx
+++ app/components/sections/Hero2.tsx
@@ -70,7 +70,7 @@ function Hero2() {
           viewport={{ once: true }}
           className="flex flex-col sm:flex-row items-center justify-center gap-4 "
         >
-          <button className="w-full sm:w-auto px-4 md:px-8 py-2 md:py-3 text-base font-medium text-white bg-indigo-600 rounded-md shadow-md hover-scale button-shadow">
+          <button className="w-full sm:w-auto px-8 py-3 text-base font-medium text-white bg-indigo-600 rounded-md shadow-md hover-scale button-shadow">
             Contact Us
           </button>
           <button className="hidden md:block w-full  px-8 py-3 text-base font-medium text-indigo-600 border border-indigo-600 rounded-md hover:bg-indigo-50 transition-colors duration-300">
