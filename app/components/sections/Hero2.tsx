b16a4bc0630a576cbff8b3d29531d905d9a246c1
joshua <joshuadsouza500@gmail.com>
2025-03-15 16:45:06 +0300

    Made some changes to hero section text

--- app/components/sections/Hero2.tsx
+++ app/components/sections/Hero2.tsx
@@ -13,7 +13,6 @@ function Hero2() {
       id="Home"
       className="md:bg-radial-gradient-custom  flex flex-col  justify-start items-start  h-auto pb-2 md:pb-8 bg-cover   bg-center bg-no-repeat mx-1  md:bg-opacity-10  rounded-2xl max-md:px-2 overflow-hidden relative"
     >
-      <div className="absolute inset-0 bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-100 via-white to-white" />
       <div className="w-full   h-[65%] py-20 flex flex-col text-center items-center bg-cover justify-center md:pt-32  2xl:pt-40   md:pb-16 2xl:pb-32  bg-opacity-30 bg-center bg-no-repeat    rounded-2xl max-md: ">
         <span className="inline-block overflow-hidden">
           <Motionh1
@@ -82,6 +81,7 @@ function Hero2() {
             Learn More
           </button>
         </MotionDiv>
+        <div className="absolute inset-0 bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-100 via-white to-white" />
       </div>
       <ProductSlider />
     </section>
